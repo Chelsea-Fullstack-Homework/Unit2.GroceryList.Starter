78f11b5f71c07ad303d5e2f60bbf657eea8ef5c5
Chelsea Scott <chelseacat62@gmail.com>
2023-10-07 22:32:59 -0700

    Implemented findItemById.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const items = [
-  { id: 1, name: "apple", price: 1.75, categoryId: 1, inventory: 100 },
+  { id: 6, name: "apple", price: 1.75, categoryId: 1, inventory: 100 },
   { id: 2, name: "banana", price: 0.25, categoryId: 1, inventory: 100 },
   { id: 3, name: "orange", price: 1.0, categoryId: 1, inventory: 100 },
   { id: 4, name: "broccoli", price: 3.0, categoryId: 2, inventory: 100 },
   { id: 5, name: "cucumber", price: 1.0, categoryId: 2, inventory: 100 },
-  { id: 6, name: "milk", price: 5.75, categoryId: 3, inventory: 100 },
+  { id: 1, name: "milk", price: 5.75, categoryId: 3, inventory: 100 },
   { id: 7, name: "cheddar cheese", price: 4.0, categoryId: 3, inventory: 100 },
   { id: 8, name: "sourdough loaf", price: 5.5, categoryId: 4, inventory: 100 },
 ];
@@ -17,7 +17,7 @@ function logItemNames() {
   //TODO: use the .forEach() method to log out the name of each item
   items.forEach((item) => {
     console.log(item.name);
-  })
+  });
 }
 
 /**
@@ -26,6 +26,11 @@ function logItemNames() {
  */
 function findItemById(id) {
   // TODO: Use the .find() method to return the item who's id matches the passed in id
+  return items.find((item) => {
+    if(id === item.id){
+      return item;
+    }
+  });
 }
 
 /**
